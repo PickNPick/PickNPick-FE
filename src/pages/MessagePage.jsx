641c23fdf8335bf81f95a8ffac06bec254ce3659
Donghyun Lee <ldhhello@naver.com>
2025-04-05 23:49:12 +0900

    ✨ 채팅 스크롤 기능 추가

--- src/pages/MessagePage.jsx
+++ src/pages/MessagePage.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import styled from 'styled-components';
 import TestImage from '../assets/testimage.png';
 import MessageTitleBox from '../components/MessageTitleBox';
 import ChatBubble from '../components/ChatBubble';
 import ChatBar from '../components/ChatBar';
 import { IoEllipseSharp } from 'react-icons/io5';
+import { theme } from "../styles/themes";
 
 const MessagePage = () => {
 
@@ -23,6 +24,7 @@ const MessagePage = () => {
     ];
 
     let [sampleMessage, setSampleMessage] = useState(SAMPLE_MESSAGE);
+    const lastRef = useRef(null);
 
     const onChat = (message) => {
         setSampleMessage([...sampleMessage, {
@@ -30,6 +32,11 @@ const MessagePage = () => {
             message
         }]);
     };
+
+    // 메시지가 업데이트될 때 아래로 스크롤
+    useEffect(() => {
+        lastRef.current.scrollIntoView();
+    }, [sampleMessage]);
 
     for(let i=0; i<sampleMessage.length; i++)
     {
@@ -39,13 +46,17 @@ const MessagePage = () => {
             sampleMessage[i].isLast = false;
     }
 
-    return <>
+    return <Container>
+        <Bannerbar>Chatting</Bannerbar>
+
         <MessageTitleBox profile={TestImage} title="이동현" subtitle="활동 중" />
 
         <MessageBox>
             <div style={{height: '25px'}} />
 
             {sampleMessage.map(({isMe, message, isLast}) => <ChatBubble isMe={isMe} profileImage={TestImage} message={message} isLast={isLast}/>)}
+
+            <div ref={lastRef} />
 
             {/* <ChatBubble isMe={true} message="달빛에 두 눈을 적셔, 내 손을 잡고 어디론가 뛰어줘." />
             <ChatBubble isMe={true} message="거짓말같았던 그 밤을 지나 끝없이 헤맨다 해도" />
@@ -61,13 +72,35 @@ const MessagePage = () => {
         </MessageBox>
 
         <ChatBar onChat={onChat} />
-    </>
+    </Container>
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`;
 
 const MessageBox = styled.div`
     display: flex;
     flex-direction: column;
     gap: 4px;
+
+    flex-grow: 1;
+    overflow-y: scroll;
+`;
+
+// 추후 밖으로 뺄 예정
+const Bannerbar = styled.div`
+font-family: 'Dela Gothic One', sans-serif;
+font-size: 15px;
+width:100%;
+height:9%;
+background-color:${theme.Sub1};
+display:flex;
+align-items:center;
+justify-content:center;
+color:white;
 `;
 
 export default MessagePage;
