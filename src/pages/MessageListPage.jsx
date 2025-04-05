cc158c082dfe9b39dd8cec24fe30b9a39399c87d
Donghyun Lee <ldhhello@naver.com>
2025-04-06 02:03:01 +0900

    🐛 useNavigate

--- src/pages/MessageListPage.jsx
+++ src/pages/MessageListPage.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import MessageListItem from '../components/MessageListItem';
 import styled from 'styled-components';
 import TestImage from '../assets/testimage.png';
+import { useNavigate } from 'react-router-dom';
 
 const MessageListPage = () => {
+    const navigate = useNavigate();
+
     return <MessageListBox>
         <MessageListItem name="사용자 이름" profile={TestImage} explain="안녕! 나는 이동현이라고 해." onClick={() => navigate('/message')} />
         <MessageListItem name="사용자 이름2" profile={TestImage} explain="ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ" />
